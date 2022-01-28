8760b4cf7dabd2e9e46990d4a36f41a68026ca4d
Round0000 <billybinche@gmail.com>
2022-01-28 13:27:10 +0100

    commit

--- app.js
+++ app.js
@@ -1,15 +1,86 @@
 let data;
 
-fetch("data/departements.json")
+const uiUserInput = document.getElementById("userInput");
+const uiResults = document.getElementById("results");
+const uiDeptView = document.getElementById("deptView");
+
+fetch("./data/departements.json")
   .then((response) => response.json())
   .then((res) => {
     data = res;
   });
 
-function displayDept(num) {
-  return data.find((dept) => dept.numero == num);
+function getResults(q) {
+  q = q.toUpperCase();
+
+  let results = [];
+
+  console.clear();
+
+  data.forEach((item) => {
+    if (
+      item.numero.toString().toUpperCase().includes(q) ||
+      item.departement.toUpperCase().includes(q) ||
+      item.region.toUpperCase().includes(q) ||
+      item.chef_lieu.toUpperCase().includes(q)
+    ) {
+      results.push(item);
+    }
+  });
+
+  return results;
 }
 
-userInput.addEventListener("change", (e) => {
-  console.log(displayDept(e.target.value));
+function displayResults(results) {
+  uiResults.innerHTML = "";
+
+  results.forEach((res) => {
+    const li = document.createElement("LI");
+    li.dataset.index = data.indexOf(res);
+    li.innerHTML = `
+      <span class="result_numero">${res.numero}</span>
+      <span class="result_departement">${res.departement}</span>
+      <span class="result_region">${res.region}</span>
+    `;
+
+    uiResults.appendChild(li);
+
+    uiDeptView.classList.add("hidden");
+    uiResults.classList.remove("hidden");
+  });
+}
+
+function displayDept(dept) {
+  uiDeptView.querySelector(".departement").innerText = dept.departement;
+  uiDeptView.querySelector(".numero").innerText = dept.numero;
+  uiDeptView.querySelector(".region").innerText = dept.region;
+
+  const uiVilles = uiDeptView.querySelector(".villes");
+  uiVilles.innerHTML = "";
+
+  const chef = document.createElement("P");
+  chef.classList.add("ville", "chef_lieu");
+  chef.innerText = dept.chef_lieu;
+
+  dept.villes.forEach((ville) => {
+    const el = document.createElement("P");
+    el.classList.add("ville");
+    el.innerText = ville;
+    uiVilles.appendChild(el);
+  });
+
+  uiResults.classList.add("hidden");
+  uiDeptView.classList.remove("hidden");
+}
+
+uiUserInput.addEventListener("keyup", (e) => {
+  displayResults(getResults(e.target.value));
+});
+
+uiResults.addEventListener("click", (e) => {
+  if (e.target.closest("li")) {
+    const query = e.target.closest("li").dataset.index;
+
+    displayDept(data[query]);
+  }
 });
